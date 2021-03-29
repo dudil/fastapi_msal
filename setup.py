7af5a9c84d33ae84bbe97327b1b9f0a8e7dddc49
dudi <dudi@elv8.co>
2021-03-29 20:41:02 +0300

    update configuration file and added pylint linting as github action

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="fastapi_msal",
-    version="0.0.1",
-    url="https://github.com/dudil/fastapi_msal",
-    description="MSAL Plugin for FastAPI",
-)
