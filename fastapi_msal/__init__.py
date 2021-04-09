763b5d9846709edeb00da46150f5cf3e43d1dbc5
dudi <dudi@elv8.co>
2021-04-09 10:27:04 +0300

    Bump into beta phase

--- fastapi_msal/__init__.py
+++ fastapi_msal/__init__.py
@@ -5,7 +5,7 @@ FastAPI - https://github.com/tiangolo/fastapi
 MSAL for Python - https://github.com/AzureAD/microsoft-authentication-library-for-python
 """
 
-__version__ = "0.0.4"
+__version__ = "0.1.0"
 from .core import MSALClientConfig
 from .models import UserInfo, IDTokenClaims
 from .auth import MSALAuthorization
