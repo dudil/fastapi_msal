1caf517f1f2dd940b30cf338a75b3c07076925cc
Dudi Levy <4785835+dudil@users.noreply.github.com>
2024-04-02 17:43:16 +0300

    Add parameter to settings, fix mypy type for URL

--- fastapi_msal/auth.py
+++ fastapi_msal/auth.py
@@ -62,13 +62,11 @@ class MSALAuthorization:
         if client_id:
             print(client_id)
         if not redirect_uri:
-            if hasattr(self.handler.client_config, 'redirect_uri'):
+            if self.handler.client_config.redirect_uri:
                 redirect_uri = self.handler.client_config.redirect_uri
             else:
-                redirect_uri = request.url_for("_get_token_route")
-        return await self.handler.authorize_redirect(
-            request=request, redirec_uri=redirect_uri, state=state
-        )
+                redirect_uri = str(request.url_for("_get_token_route"))
+        return await self.handler.authorize_redirect(request=request, redirec_uri=redirect_uri, state=state)
 
     async def _get_token_route(self, request: Request, code: str, state: OptStr) -> RedirectResponse:
         await self.handler.authorize_access_token(request=request, code=code, state=state)
