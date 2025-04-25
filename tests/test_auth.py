c98166a68c56bb0ce10135549eac0364bf8021cc
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2025-04-25 10:28:44 -0500

    Add test for return_to_path

---
+++ tests/test_auth.py
@@ -0,0 +1,27 @@
+from urllib.parse import parse_qs, urlparse
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from starlette.middleware.sessions import SessionMiddleware
+
+from fastapi_msal import MSALAuthorization, MSALClientConfig
+
+
+@pytest.fixture
+def dummy_client_config():
+    return MSALClientConfig()
+
+
+def test_return_to_path(dummy_client_config):
+    auth = MSALAuthorization(client_config=dummy_client_config, return_to_path="https://www.example.com")
+
+    app = FastAPI()
+    app.add_middleware(SessionMiddleware, secret_key="")
+    app.include_router(auth.router)
+    client = TestClient(app, follow_redirects=False)
+    response = client.get(app.url_path_for("_logout_route"))
+    assert response.is_redirect
+
+    redirect = urlparse(response.headers.get("Location"))
+    assert parse_qs(redirect.query)["post_logout_redirect_uri"][0] == "https://www.example.com"
